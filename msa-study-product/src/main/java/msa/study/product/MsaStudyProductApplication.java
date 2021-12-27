6385c80e2ae383ad19936ce476601e9a66a675f6
ChoiSangIl <wo566@naver.com>
2021-12-28 04:31:20 +0900

    상품관련 프로젝트 생성

---
+++ msa-study-product/src/main/java/msa/study/product/MsaStudyProductApplication.java
@@ -0,0 +1,13 @@
+package msa.study.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaStudyProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaStudyProductApplication.class, args);
+	}
+
+}
