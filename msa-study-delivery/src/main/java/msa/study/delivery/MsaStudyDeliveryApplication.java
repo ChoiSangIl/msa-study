fdec27650e8fbcd4758f16b8c12c07eff890231c
ChoiSangIl <wo566@naver.com>
2021-12-28 04:34:55 +0900

    배송관련 프로젝트 생성

---
+++ msa-study-delivery/src/main/java/msa/study/delivery/MsaStudyDeliveryApplication.java
@@ -0,0 +1,13 @@
+package msa.study.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaStudyDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaStudyDeliveryApplication.class, args);
+	}
+
+}
