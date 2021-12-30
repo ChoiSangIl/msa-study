7ffa8855a756cfbf00cba2822ff54d98b4fea86d
ChoiSangIl <wo566@naver.com>
2021-12-30 22:07:50 +0900

    포트 추가 및 resttemplate 테스트코드 추가

---
+++ msa-study-order/src/test/java/msa/study/order/service/impl/OrderServiceImplTest.java
@@ -0,0 +1,54 @@
+package msa.study.order.service.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.client.ExpectedCount;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootTest
+public class OrderServiceImplTest {
+
+	@Autowired
+	private RestTemplate template;
+	
+	private MockRestServiceServer mockServer;
+	
+	@BeforeEach
+	public void init() throws URISyntaxException {
+		mockServer = MockRestServiceServer.createServer(template);
+		mockServer.expect(ExpectedCount.once(), 
+		          requestTo(new URI("http://localhost:8083/product/stock/order")))
+		          .andExpect(method(HttpMethod.PUT))
+		          .andRespond(withStatus(HttpStatus.OK)
+		          .contentType(MediaType.APPLICATION_JSON)
+		          .body("minus stock..."));            
+	}
+	
+	@Test
+	public void testOrder() throws URISyntaxException {
+	    HttpHeaders headers = new HttpHeaders();
+	    headers.setContentType(MediaType.APPLICATION_JSON);
+	    HttpEntity<String> entity = new HttpEntity<String>("", headers); 
+	    
+		ResponseEntity<String> response = template.exchange("http://localhost:8083/product/stock/order", HttpMethod.PUT, entity, String.class);
+		assertEquals("minus stock...", response.getBody());
+	}
+
+}
