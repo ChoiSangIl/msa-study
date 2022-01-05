b4a958697ac0e15732948aa2ac8ba727d20c3147
ChoiSangIl <wo566@naver.com>
2022-01-05 17:05:51 +0900

    eureka init

---
+++ msa-study-service-discovery/src/main/java/msa/study/delivery/MsaStudyServiceDiscoveryApplication.java
@@ -0,0 +1,15 @@
+package msa.study.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MsaStudyServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaStudyServiceDiscoveryApplication.class, args);
+	}
+
+}
---
+++ msa-study-service-discovery/src/test/java/msa/study/delivery/MsaStudyServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package msa.study.delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaStudyServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
