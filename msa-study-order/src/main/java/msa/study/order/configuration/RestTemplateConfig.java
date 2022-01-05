6071eaaad9eb875cdb852a112aaf92fd894877aa
ChoiSangIl <wo566@naver.com>
2022-01-05 13:22:37 +0900

    resttemplate timeout 10초로 변경

--- msa-study-order/src/main/java/msa/study/order/configuration/RestTemplateConfig.java
+++ msa-study-order/src/main/java/msa/study/order/configuration/RestTemplateConfig.java
@@ -18,7 +18,7 @@ public class RestTemplateConfig {
         return restTemplateBuilder
                 .requestFactory(() -> new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()))
                 .setConnectTimeout(Duration.ofMillis(5000)) // connection-timeout
-                .setReadTimeout(Duration.ofMillis(5000)) // read-timeout
+                .setReadTimeout(Duration.ofMillis(10000)) // read-timeout
                 .additionalMessageConverters(new StringHttpMessageConverter(Charset.forName("UTF-8")))
                 .build();
     }
