3de102656571a6cd7d0d142974657cd12bcaf3a0
ChoiSangIl <wo566@naver.com>
2022-01-10 12:25:22 +0900

    feign 추가

--- msa-study-order/src/main/java/msa/study/order/MsaStudyOrderApplication.java
+++ msa-study-order/src/main/java/msa/study/order/MsaStudyOrderApplication.java
@@ -3,9 +3,11 @@ package msa.study.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class MsaStudyOrderApplication {
 
 	public static void main(String[] args) {
