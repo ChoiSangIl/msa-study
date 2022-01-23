c7c275af67dcdcea88d170f3ac7892a5f98dc21a
ChoiSangIl <wo566@naver.com>
2022-01-23 15:00:32 +0900

    time.sleep 추가

--- msa-study-pay/src/main/java/msa/study/pay/controller/api/PayController.java
+++ msa-study-pay/src/main/java/msa/study/pay/controller/api/PayController.java
@@ -1,5 +1,6 @@
 package msa.study.pay.controller.api;
 
+import org.springframework.cloud.client.circuitbreaker.NoFallbackAvailableException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,12 @@ public class PayController {
 
 	@PostMapping("/pay")
 	public String pay() {
-		return "pay...";
+		try {
+			Thread.sleep(60000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		throw new NoFallbackAvailableException("Boom!", new RuntimeException());
 	}
 }
