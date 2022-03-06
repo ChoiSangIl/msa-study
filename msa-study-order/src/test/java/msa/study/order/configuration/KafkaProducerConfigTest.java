8eef344980d63b7afe69adb06a437f53c2589bce
ChoiSangIl <wo566@naver.com>
2022-03-06 13:45:29 +0900

    pay kafka consumer 추가

--- msa-study-order/src/test/java/msa/study/order/configuration/KafkaProducerConfigTest.java
+++ msa-study-order/src/test/java/msa/study/order/configuration/KafkaProducerConfigTest.java
@@ -1,21 +1,35 @@
 package msa.study.order.configuration;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 
-@SpringBootTest
 public class KafkaProducerConfigTest {
 
-	@Autowired
-	KafkaTemplate<?, String> kafkaTemplate;
+	KafkaTemplate<String, String> kafkaTemplate;
 	
 	@Test
 	public void kafkaConnectTest() {
-		assertNotNull(kafkaTemplate);
-		this.kafkaTemplate.send("test", "123");
+		kafkaTemplate = kafkaTemplate();	
+		System.out.println("send...");
+		kafkaTemplate.send("payRequest", "test");
 	}
+	
+    public ProducerFactory<String, String> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put( ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "http://localhost:9092");
+        configProps.put( ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put( ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+    
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
 }
