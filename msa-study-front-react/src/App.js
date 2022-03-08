5b9d51854aefa3f165eea2f1ad05edcb14c55d02
ChoiSangIl <wo566@naver.com>
2022-03-09 00:44:05 +0900

    react-bootstrap 적용

--- msa-study-front-react/src/App.js
+++ msa-study-front-react/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container-fluid">
+      <Button variant="primary">Button #1</Button>
+      <Button variant="secondary" className="mx-2">Button #2</Button>
+      <Button variant="success">Button #3</Button>
     </div>
   );
 }
