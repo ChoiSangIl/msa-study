1103d67728e0ba2fb7758f5888ef58859a3995c7
ChoiSangIl <wo566@naver.com>
2022-04-25 18:55:31 +0900

    feat: 더미상품 20개씩 추가되도록 수정

--- msa-study-product/src/main/java/msa/study/product/command/DumyProductCreator.java
+++ msa-study-product/src/main/java/msa/study/product/command/DumyProductCreator.java
@@ -1,5 +1,8 @@
 package msa.study.product.command;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -18,10 +21,9 @@ public class DumyProductCreator implements CommandLineRunner{
 	@Autowired
 	private ProductDetailRepository productDetailRepository;
 	
-	private static final int price = 2000;
 	private static final String name = "test상품";
-	private static final String thumbnailUrl = "/thumbnail/1.jpg";
-	private static final String productDetail = "해당상품 좋아요:)";
+	private static final String thumbnailUrl = "/thumbnail/";
+	private static final String productDetail = "상품 상세";
 
 	@Autowired
 	public DumyProductCreator(ProductRepository productRepository, ProductDetailRepository productDetailRepository) {
@@ -36,20 +38,29 @@ public class DumyProductCreator implements CommandLineRunner{
 		 * 동시에 서비스가 동시에 올라가면 테스트 상품이 여러개 등록 될 수 있다. (실제 서비스에서는 주석처리하거나, dev, real로 나눠서 처리 해야함)
 		 */
 		if(productRepository.findAll().isEmpty()) {
-			Product product = Product.builder()
-					.price(price)
-					.name(name)
-					.thumbnailUrl(thumbnailUrl)
-					.build();
+			
+			final int dumyProductCount = 20;
+			List<Product> products = new ArrayList<Product>();
+			List<ProductDetail> details = new ArrayList<ProductDetail>();
+			
+			for (int i=1; i<= dumyProductCount; i++) {
+				Product product = Product.builder()
+						.price((int)(Math.random() * 99000) + 1000)
+						.name(name + i)
+						.thumbnailUrl(thumbnailUrl + i + ".jpg")
+						.build();
 
-			ProductDetail detail = ProductDetail.builder()
-								.detail(productDetail)
-								.product(product)
-								.build();
+				ProductDetail detail = ProductDetail.builder()
+									.detail(i + " " + productDetail)
+									.product(product)
+									.build();
+				
+				products.add(product);
+				details.add(detail);
+			}
 			
-			productRepository.save(product);
-			productDetailRepository.save(detail);
-			
+			productRepository.saveAll(products);
+			productDetailRepository.saveAll(details);
 		}
 	}
 
