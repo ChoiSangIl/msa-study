c3906e97b44283da0c5cb3f59d5ac80534aa9e63
ChoiSangIl <wo566@naver.com>
2022-04-25 16:49:56 +0900

    test: 주문 api 호출 테스트코드 수정

--- msa-study-order/src/test/java/msa/study/order/controller/api/OrderControllerTest.java
+++ msa-study-order/src/test/java/msa/study/order/controller/api/OrderControllerTest.java
@@ -1,51 +1,46 @@
 package msa.study.order.controller.api;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import msa.study.order.service.OrderService;
 
+@WebMvcTest(controllers = OrderController.class)
 public class OrderControllerTest {
 	
+	@Autowired
 	private MockMvc mockMvc;
 	
+	@MockBean
 	OrderService orderService;
-	OrderController orderController;
-	
-	@BeforeEach
-	private void init() {
-		this.orderService = mock(OrderService.class); 
-		doReturn("orderComplete").when(orderService).orders();
-		this.orderController = new OrderController(orderService);
-		this.mockMvc = MockMvcBuilders.standaloneSetup(orderController).build();
-	}
 	 
 	@Test
+	@DisplayName("주문 API를 호출할 수 있다.")
 	void testOrder() throws Exception {
-		assertNotNull(mockMvc);
+		//given
+		doReturn("orderComplete").when(orderService).orders();
 		
-		MvcResult mvcResult = mockMvc.perform(post("/orders"))
-			.andExpect(status().isOk())
-			.andDo(print())
-			.andReturn();
-
+		//when
+		MvcResult mvcResult = mockMvc.perform(
+				post("/orders")
+		)
+		
+		//then
+		.andExpect(status().isOk())
+		.andDo(print())
+		.andReturn();
 		assertEquals("orderComplete", mvcResult.getResponse().getContentAsString());
-	}
-
-	@Test
-	public void testGetOrders() {
-		
 	}
 
 }
