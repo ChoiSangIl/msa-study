e6da4cb52a11711bbef959ee1cd9b0f6c62a86b7
ChoiSangIl <wo566@naver.com>
2022-05-03 16:46:25 +0900

    feat: api gateway springdoc-opeanapi-ui 적용

---
+++ msa-study-gateway/src/main/java/msa/study/gateway/filter/SwaggerHeaderFilter.java
@@ -0,0 +1,8 @@
+package msa.study.gateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.server.ServerWebExchange;
