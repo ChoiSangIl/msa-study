06bef1025c62389bb2937fbccf80c98158515cc0
ChoiSangIl <wo566@naver.com>
2022-05-05 01:14:23 +0900

    refactor: kafka 설정방식 class로 변경

---
+++ msa-study-pay/src/test/java/msa/study/pay/service/PayRequestConsumerTest.java
@@ -0,0 +1,16 @@
+package msa.study.pay.service;
+
+import org.junit.jupiter.api.Test;
+
+public class PayRequestConsumerTest {
+	
+	public void initData() {
+		
+	}
+
+	@Test
+	public void testConsume() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+}
