01552949625922b45158efe9cca043e9c5e0073c
ChoiSangIl <wo566@naver.com>
2022-05-04 15:51:59 +0900

    feat: swagger server api gateway url로 변경

--- msa-study-order/src/main/java/msa/study/order/configuration/SwaggerConfig.java
+++ msa-study-order/src/main/java/msa/study/order/configuration/SwaggerConfig.java
@@ -1,18 +1,34 @@
 package msa.study.order.configuration;
 
+import java.util.List;
+
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+
 @Configuration
 public class SwaggerConfig {
+	
+	private static final String GATEWAY_URL = "http://localhost:8080";
 
-	 @Bean
-	  public GroupedOpenApi publicApi() {
-	      return GroupedOpenApi.builder()
-	              .group("springshop-public")
-	              .pathsToMatch("/**")
-	              .build();
-	  }
+	@Bean
+	public GroupedOpenApi publicApi() {
+		return GroupedOpenApi.builder().group("springshop-public").pathsToMatch("/**").build();
+	}
+
+	/**
+	 * swagger 호출 api를 gateway로 설정해주기 위함
+	 * 실제 운영환경에서는 운영환경의 GATEWAY 주소로 변경되어야함
+	 * @return
+	 */
+	@Bean
+	public OpenAPI customOpenAPI() {
+		Server server = new Server();
+		server.setUrl(GATEWAY_URL);
+		return new OpenAPI().servers(List.of(server));
+	}
 
 }
