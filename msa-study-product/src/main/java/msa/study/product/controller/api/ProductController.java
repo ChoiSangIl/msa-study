9cda1d6be2632be5912570b0eb99463b040a7415
ChoiSangIl <wo566@naver.com>
2022-05-05 02:07:50 +0900

    feat: product domain swagger적용 및 상품 검색 api 설명 추가

--- msa-study-product/src/main/java/msa/study/product/controller/api/ProductController.java
+++ msa-study-product/src/main/java/msa/study/product/controller/api/ProductController.java
@@ -6,12 +6,15 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import msa.study.product.controller.dto.ProductListRequest;
 import msa.study.product.controller.dto.ProductListResponse;
 import msa.study.product.service.ProductService;
 
 @RestController
 @RequestMapping("/product")
+@Tag(name = "상품관련 API")
 public class ProductController {
 
 	private final ProductService productService;
@@ -22,11 +25,13 @@ public class ProductController {
 	}
 
 	@PutMapping("/stock/order")
+	@Operation(summary = "재고차감")
 	private String minusStock() {
 		return "minus stock...";
 	}
 	
 	@GetMapping("/search")
+	@Operation(summary = "상품검색")
 	private ProductListResponse getProductList(ProductListRequest productListRequest) {
 		return productService.getProductList(productListRequest);
 	}
--- msa-study-product/src/main/java/msa/study/product/controller/dto/ProductListRequest.java
+++ msa-study-product/src/main/java/msa/study/product/controller/dto/ProductListRequest.java
@@ -1,10 +1,20 @@
 package msa.study.product.controller.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * 
+ * 상품 검색 VO
+ * @author Choi Sang Il
+ *
+ */
 @Getter
 @Setter
 public class ProductListRequest {
+
+	@Schema(description = "상품명")
 	private String productName;
+	
 }
