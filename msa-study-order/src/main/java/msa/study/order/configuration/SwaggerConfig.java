119ef2a94f77e3dc7a1a328f43f2c6312f9c0e0e
ChoiSangIl <wo566@naver.com>
2022-05-09 12:56:58 +0900

    fix: modify microservice swagger setting

--- msa-study-order/src/main/java/msa/study/order/configuration/SwaggerConfig.java
+++ msa-study-order/src/main/java/msa/study/order/configuration/SwaggerConfig.java
@@ -1,13 +1,13 @@
 package msa.study.order.configuration;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
@@ -16,12 +16,15 @@ public class SwaggerConfig {
 	private static final String GATEWAY_URL = "http://localhost:8080/order-service";
 
 	@Bean
-	public GroupedOpenApi publicApi() {
+	public OpenAPI springOpenAPI() {
 		Server server = new Server();
 		server.setUrl(GATEWAY_URL);
 		List<Server> list = new ArrayList<Server>();
 		list.add(server);
-		
-		return GroupedOpenApi.builder().group("msa-order").pathsToMatch("/**").addOpenApiCustomiser(openApi -> openApi.servers(list)).build();
+
+		return new OpenAPI()
+				.info(new Info().title("msa-study-order").description("msa-study-order sample application").version("v0.0.1"))
+				.servers(list);
 	}
+
 }
--- msa-study-product/src/main/java/msa/study/product/config/SwaggerConfig.java
+++ msa-study-product/src/main/java/msa/study/product/config/SwaggerConfig.java
@@ -7,21 +7,25 @@ import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
-	
+
 	private static final String GATEWAY_URL = "http://localhost:8080/product-service";
 
 	@Bean
-	public GroupedOpenApi publicApi() {
+	public OpenAPI springOpenAPI() {
 		Server server = new Server();
 		server.setUrl(GATEWAY_URL);
 		List<Server> list = new ArrayList<Server>();
 		list.add(server);
-		
-		return GroupedOpenApi.builder().group("msa-order").pathsToMatch("/**").addOpenApiCustomiser(openApi -> openApi.servers(list)).build();
+
+		return new OpenAPI()
+				.info(new Info().title("msa-study-product").description("msa-study-product sample application").version("v0.0.1"))
+				.servers(list);
 	}
 
 }
