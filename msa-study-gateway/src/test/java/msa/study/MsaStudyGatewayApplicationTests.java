95452b16de7cc86b4b7ad72bd5fff07a79a4f37b
ChoiSangIl <wo566@naver.com>
2022-05-19 14:11:36 +0900

    feat: apigateway add docker config file

--- msa-study-gateway/src/test/java/msa/study/MsaStudyGatewayApplicationTests.java
+++ msa-study-gateway/src/test/java/msa/study/MsaStudyGatewayApplicationTests.java
@@ -1,13 +1,8 @@
 package msa.study;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MsaStudyGatewayApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
